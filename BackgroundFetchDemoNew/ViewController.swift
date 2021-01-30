2bf58aa8ec201622846c520a998c55e37e56f945
sapreashu@gmail.com <himanshu.gandhi.knoxpo@gmail.com>
2021-01-30 13:54:08 +0530

    show current time

--- BackgroundFetchDemoNew/ViewController.swift
+++ BackgroundFetchDemoNew/ViewController.swift
@@ -8,12 +8,31 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var timLbel: UILabel!
+    var currentTime: Date?
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+       // self.updateTime()
         // Do any additional setup after loading the view.
     }
 
+    func updateTime()
+    {
+        
+        currentTime = Date()
+        let formatter = DateFormatter()
+        formatter.timeStyle = .short
+        if let currentTime = currentTime {
+            
+            timLbel.text = formatter.string(from: currentTime)
+        }
+        
+    }
+    
+    
 
 }
 
