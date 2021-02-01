b4c883deb060bcaf2b05ea41c8f4487974eb4a73
sapreashu@gmail.com <himanshu.gandhi.knoxpo@gmail.com>
2021-02-01 18:43:30 +0530

    shwow date and time in background

--- BackgroundFetchDemoNew/AppDelegate.swift
+++ BackgroundFetchDemoNew/AppDelegate.swift
@@ -7,13 +7,26 @@
 
 import UIKit
 
-@main
+@UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
+    
+    private let userdefault = UserDefaults.standard
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        UIApplication.shared.setMinimumBackgroundFetchInterval(UIApplication.backgroundFetchIntervalMinimum)
+        
+        UINavigationBar.appearance().isTranslucent = false
+        UINavigationBar.appearance().barTintColor = UIColor.white
+        UINavigationBar.appearance().tintColor = UIColor.white
+        
+        window = UIWindow(frame: UIScreen.main.bounds)
+        window?.rootViewController = UINavigationController(rootViewController: ViewController())
+        window?.makeKeyAndVisible()
+        
+        userdefault.register(defaults: ["Log": "date"])
+        application.setMinimumBackgroundFetchInterval(UIApplication.backgroundFetchIntervalMinimum)
+    //    UIApplication.shared.setMinimumBackgroundFetchInterval(UIApplication.backgroundFetchIntervalMinimum)
         // Override point for customization after application launch.
         return true
     }
@@ -21,10 +34,31 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
    
     func application(_ application: UIApplication, performFetchWithCompletionHandler completionHandler: @escaping (UIBackgroundFetchResult) -> Void) {
         
-        if let viewcontroller = window?.rootViewController as? ViewController {
-            viewcontroller.updateTime()
-        }
+//        if let viewcontroller = window?.rootViewController as? ViewController {
+//            viewcontroller.updateTime()
+//        }
         
+        let now = NSDate()
+        let formatter = DateFormatter()
+        formatter.dateFormat = "yyyy/mm/dd hh:mm:ss"
+        let currenttime = formatter.string(from: now as Date)
+        let rec = userdefault.object(forKey: "Log") as! String + "\n" + currenttime
+        userdefault.set(rec , forKey: "Log")
+        userdefault.synchronize()
+        print(rec)
+        
+        
+        
+        
+        
+//        if  let tabbarItem = window?.rootViewController as? ViewController {
+//            print("hiiio")
+//            tabbarItem.updateTime()
+//
+//
+//        }
+        
+        completionHandler(UIBackgroundFetchResult.newData)
     }
     
    
--- BackgroundFetchDemoNew/ViewController.swift
+++ BackgroundFetchDemoNew/ViewController.swift
@@ -8,13 +8,38 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    private let userdefaut = UserDefaults.standard
     @IBOutlet weak var timLbel: UILabel!
     var currentTime: Date?
+    
+    
+    private let label: UILabel = {
+        
+        let label = UILabel()
+        label.textColor = .white
+        label.backgroundColor = .blue
+        label.numberOfLines = 10
+        return label
+        
+    }()
+  
     
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        
+        view.backgroundColor = .gray
+        label.text = userdefaut.object(forKey: "Log") as? String
+        view.addSubview(label)
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        label.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
+        
+        
+        
        // self.updateTime()
         // Do any additional setup after loading the view.
     }
